3f99b3fd22ecb803c39168850fbedf42bf7717d0
Rohitchaudhari-mobile <rohit.c@amrutsoftware.com>
2025-01-24 18:22:59 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,32 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+
+
+const App=()=> {
+
+
+  return (
+<View><Text>jhifbkhjfjkjkjkjf</Text></View>
+  );
+}
+
+
+export default App;
