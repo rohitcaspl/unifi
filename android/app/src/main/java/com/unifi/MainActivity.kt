207cf90ebdcdd97552de4d6aa105e71a69b60df2
rohitcaspl <rohit.c@amrutsoftware.com>
2025-01-31 12:55:17 +0530

    3 packages are remaining

--- android/app/src/main/java/com/unifi/MainActivity.kt
+++ android/app/src/main/java/com/unifi/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.unifi
-
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+
 
 class MainActivity : ReactActivity() {
 
